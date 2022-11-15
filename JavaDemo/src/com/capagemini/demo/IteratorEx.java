87cc62664e058c38ba61d5627b52411b0b4f1862
sarva <sarva@LAPTOP-N5G87EA0>
2022-11-15 19:39:51 +0530

    fourteenth commit

---
+++ JavaDemo/src/com/capagemini/demo/IteratorEx.java
@@ -0,0 +1,38 @@
+package com.capagemini.demo;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class IteratorEx {
+	void display(LinkedList<String> list) {
+		Iterator<String> it=list.iterator();
+		while(it.hasNext()) {
+			System.out.println("Player name is : "+it.next());
+		}
+	}
+	public static void main(String[] args) {
+		LinkedList<String> player=new LinkedList<>();
+		player.add("Dhoni");
+		player.add("Virat");
+		player.add("KL Rahul");
+		player.add("Sachin");
+		player.add("Rohit");
+		player.peek();
+		player.set(3, "Hardik");
+	
+		
+		System.out.println(player.clone());
+		System.out.println(player.get(3));
+		System.out.println(player.containsAll(player));
+		System.out.println(player.offerFirst("Virat"));
+		System.out.println(player.hashCode());
+		System.out.println(player.poll());
+		System.out.println(player.descendingIterator());
+		System.out.println(player);
+		
+		IteratorEx ie=new IteratorEx();
+		ie.display(player);
+		
+	}
+
+}
